1b09feccf6cb8234b47b7eca72f515aa83eca095
mo1nammad <mhaagrm2@gmail.com>
2025-11-13 17:45:43 +0330

    add package json lock and configuration vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 export default defineConfig({
-  server: {
-    host: true,
-    strictPort: true,
-    port: 5173,
-  },
+  // for docker
+
+  // server: {
+  //   host: true,
+  //   strictPort: true,
+  //   port: 5173,
+  // },
   base: "/waving-pattern/",
   plugins: [glsl()],
 });
